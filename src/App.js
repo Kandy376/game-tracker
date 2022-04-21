03909d5b50ede3f239ee00c09c7abedde524b9e1
Kandy <davidfeldman1234hi@gmail.com>
2022-04-21 19:01:40 +1000

    Created new folders, and card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/> 
-      <div className=''>main</div>
+      <Main/>
       <div>footer</div>
     </div>
   );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Card from './Card'
+
+
+function Main() {
+  return (
+    <>
+      <Card/>
+    </>
+  )
+}
+
+export default Main
