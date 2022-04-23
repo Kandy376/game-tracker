2943f0abd5e489a01179a82e85509324c1a72896
Kandy <davidfeldman1234hi@gmail.com>
2022-04-23 18:57:30 +1000

    created json server for fake info

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import logo from '../../logo.svg'
 
 function Card() {
   return (
-    <div>Card</div>
+    <div>
+      <h1>Valorant</h1>
+      <img src={logo}/>
+    </div>
   )
 }
+
+// use tailwind to make card design, use Navbar as reference.
 
 export default Card
