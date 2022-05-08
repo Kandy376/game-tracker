599d34eae76a476420382c314b78ba38b38e188d
Kandy <davidfeldman1234hi@gmail.com>
2022-05-08 16:12:21 +1000

    created basic backend and requests to it

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -5,11 +8,20 @@ import Main from './components/Main/Main';
 
 
 function App() {
+const [games, setGames]=useState([])
+useEffect(()=>{
+  axios.get('http://localhost:3001/games').then(response => {
+    setGames(response.data)
+  })
+},[])
+console.log("games", games)
+const item=games?games[0].title:"nothing to show"
+
   return (
     <div className="App">
       <Navbar/> 
       <Main/>
-      <div>footer</div>
+      <div>{item}</div>
     </div>
   );
 }
